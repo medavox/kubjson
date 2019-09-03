7469d6460d1d0b5c95839f51e16980d517f5a099
Adam Howard <achdisposable1+github@gmail.com>
2019-09-03 20:38:34 +0100

    test: add skeleton for CodecTests

---
+++ src/test/kotlin/CodecTests.kt
@@ -0,0 +1,56 @@
+import org.junit.Test
+
+class CodecTests {
+    @Test
+    fun nullTest() {
+        
+    }
+    @Test
+    fun noopTest() {
+        TODO()
+    }
+    @Test
+    fun booleanTest() {
+        TODO()
+    }
+    @Test
+    fun int8Test() {
+        TODO()
+    }
+    @Test
+    fun uint8Test() {
+        TODO()
+    }@Test
+    fun int16Test() {
+        TODO()
+    }
+    @Test
+    fun int32Test() {
+        TODO()
+    }
+    @Test
+    fun int64Test() {
+        TODO()
+    }
+    @Test
+    fun float32Test() {
+        TODO()
+    }
+    @Test
+    fun float64Test() {
+        TODO()
+    }
+    @Test
+    fun charTest() {
+        TODO()
+    }
+    @Test
+    fun stringTest() {
+        TODO()
+    }
+    @Test
+    fun highPrecisionNumberTest() {
+        TODO()
+    }
+
+}
